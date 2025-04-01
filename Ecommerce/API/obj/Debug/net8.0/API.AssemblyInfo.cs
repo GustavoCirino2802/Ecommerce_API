d184896e92dcc57fb1c61c6877868fb4bf99ff14
GUSTAVO DE ALMEIDA CIRINO <gustavo.cirino@cs.up.edu.br>
2025-03-31 21:27:03 -0300

    Conexao com DB

--- Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fd4ebfc1f63ad59fdbf095e77e7358d6802cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
