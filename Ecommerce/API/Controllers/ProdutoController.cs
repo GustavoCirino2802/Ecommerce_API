f84da92549b67ba6ec6f9465362bbdd8de914f75
GUSTAVO DE ALMEIDA CIRINO <gustavo.cirino@cs.up.edu.br>
2025-03-31 21:46:47 -0300

    Cadastro de item

--- Ecommerce/API/Controllers/ProdutoController.cs
+++ Ecommerce/API/Controllers/ProdutoController.cs
@@ -1,3 +1,5 @@
+using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -7,6 +9,20 @@ namespace API.Controllers;
 
 public class ProdutoController : ControllerBase
 {
+    private readonly AppDataContext _context;
+    public ProdutoController(AppDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar([FromBody] Produto produto)
+    {
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return Created("", produto);
+    }
+
     //Exemplo de método dentro de uma classe
     [HttpGet]
     public string Metodo()
